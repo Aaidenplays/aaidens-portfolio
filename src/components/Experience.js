529b9f445791e3abd24a9460a0c2930053fe5e40
aaidenplays <aaidenplays@yahoo.com>
2020-07-09 16:55:59 -0500

    created divs for experience descriptions

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,14 +6,26 @@ export class Experience extends Component {
                 <div className="experience-container">
                     <h1><a id='Experience' className="row exp-title" >Experience</a></h1>
                     <div className="row exp-rw">
+                            <div className="exp-desc-left">
+
+                            </div>
                             <img className="exp-pic col" src={require("../../static/assets/pictures/Etudes-logo.png")}/>
                             <img className="exp-pic col" src={require("../../static/assets/pictures/Flatiron-logo.jpg")}/>
+                            <div className="exp-desc-right">
+
+                            </div>
                     </div>
                     <div className="row exp-rw">
+                            <div className="exp-desc-left">
+
+                            </div>
                             <img className="exp-pic col" src={require("../../static/assets/pictures/UTSA-logo.jpg")}/>
                             <img className="exp-pic col" src={require("../../static/assets/pictures/Terry-logo.png")}/>
+                            <div className="exp-desc-right">
+
+                            </div>
                     </div>
-                </div>
+                </div> 
         )
     }
 }
